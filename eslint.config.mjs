fbe4708d61834f36b1c8cef2cd955892058ad928
toothlessdev <toothless042@gmail.com>
2026-01-10 21:14:32 +0900

    chore: eslint-plugin-unused-imports 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import prettierRecommended from "eslint-plugin-prettier/recommended";
 import globals from "globals";
+import unusedImports from "eslint-plugin-unused-imports";
 
 export default tseslint.config(
     {
@@ -29,13 +30,23 @@ export default tseslint.config(
                 tsconfigRootDir: import.meta.dirname,
             },
         },
+        plugins: {
+            "unused-imports": unusedImports,
+        },
         rules: {
             "@typescript-eslint/no-explicit-any": "warn",
-            "@typescript-eslint/no-unused-vars": [
-                "warn",
-                { argsIgnorePattern: "^_" },
-            ],
+            "@typescript-eslint/no-unused-vars": "off",
             "@typescript-eslint/explicit-function-return-type": "off",
+            "unused-imports/no-unused-imports": "error",
+            "unused-imports/no-unused-vars": [
+                "warn",
+                {
+                    vars: "all",
+                    varsIgnorePattern: "^_",
+                    args: "after-used",
+                    argsIgnorePattern: "^_",
+                },
+            ],
         },
     },
     {
