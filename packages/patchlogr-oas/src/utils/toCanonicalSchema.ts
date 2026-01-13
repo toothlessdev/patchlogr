eedfcd5ee00b014049d0f6cc1d969da8b3d9d479
toothlessdev <toothless042@gmail.com>
2026-01-13 17:33:55 +0900

    refactor: 불필요한 타입 단언 제거

--- packages/patchlogr-oas/src/utils/toCanonicalSchema.ts
+++ packages/patchlogr-oas/src/utils/toCanonicalSchema.ts
@@ -1,5 +1,4 @@
 import { type CanonicalSchema } from "@patchlogr/types";
-import type { OpenAPIV2, OpenAPIV3 } from "openapi-types";
 import type { OpenAPISchemaObject } from "../guards/schemaGuards";
 import { isSchemaObject, isOpenAPIV3Schema } from "../guards/schemaGuards";
 import { toCanonicalSchemaV2 } from "./toCanonicalSchemaV2";
